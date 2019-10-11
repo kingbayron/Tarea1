9473e000a1bc8e4dbf9852f32ceff559fe8508d2
kingbayron <kingbayron@gmail.com>
2019-10-11 12:40:32 -0300

    Cambios en migrations

--- db/migrate/20191006232228_add_columns_to_users.rb
+++ db/migrate/20191006232228_add_columns_to_users.rb
@@ -1,5 +1,9 @@
 class AddColumnsToUsers < ActiveRecord::Migration[6.0]
   def change
     add_reference :users, :curso, null: true, foreign_key: true
+    add_column :users, :nombre, :string
+    add_column :users, :apellido, :string
+    add_column :users, :username, :string
+    add_column :users, :tipouser, :integer
   end
 end
